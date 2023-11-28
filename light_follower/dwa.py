f7b04a986b07de4af70f9affac68b67429fbcde0
MasakiFujiwara <fmasaki2020@gmail.com>
2023-11-28 22:52:40 +0900

    update dwa

--- light_follower/dwa.py
+++ light_follower/dwa.py
@@ -1,11 +1,4 @@
-"""
-
-Mobile robot motion planning sample with Dynamic Window Approach
-
-author: Atsushi Sakai (@Atsushi_twi)
-
-"""
-
+#! /usr/bin/env python3
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,141 +22,158 @@ class Config():
         self.predict_time = 3.0  # [s]
         self.to_goal_cost_gain = 1.0
         self.speed_cost_gain = 1.0
-        self.robot_radius = 0.18  # [m]
+        self.robot_radius = 0.5  # [m]
+
+class dwa_approach():
+    def __init__(self, NUM_ROBOT=4, GOAL=np.array([10, 10]), X=np.array([0.0, 0.0, math.pi / 8.0, 0.0, 0.0])):
+        self.num_robots = NUM_ROBOT
+        self.config = Config()
+
+        # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
+        self.x = X
+        # goal position [x(m), y(m)]
+        self.goal = GOAL
+        # obstacles [x(m) y(m), ....]
+        self.ob = np.matrix(np.arange(0., (self.num_robots-1)*2.).reshape((self.num_robots-1), 2))
+        # control u [linear.x, angular.z]
+        self.u = np.array([0.0, 0.0])
+
+    def motion(x, u, dt):
+        # motion model
+
+        x[2] += u[1] * dt
+        x[0] += u[0] * math.cos(x[2]) * dt
+        x[1] += u[0] * math.sin(x[2]) * dt
+        x[3] = u[0]
+        x[4] = u[1]
+
+        return x
 
 
-def motion(x, u, dt):
-    # motion model
+    def calc_dynamic_window(self, x, config):
 
-    x[2] += u[1] * dt
-    x[0] += u[0] * math.cos(x[2]) * dt
-    x[1] += u[0] * math.sin(x[2]) * dt
-    x[3] = u[0]
-    x[4] = u[1]
+        # Dynamic window from robot specification
+        Vs = [config.min_speed, config.max_speed,
+            -config.max_yawrate, config.max_yawrate]
 
-    return x
+        # Dynamic window from motion model
+        Vd = [x[3] - config.max_accel * config.dt,
+            x[3] + config.max_accel * config.dt,
+            x[4] - config.max_dyawrate * config.dt,
+            x[4] + config.max_dyawrate * config.dt]
+        #  print(Vs, Vd)
 
+        #  [vmin,vmax, yawrate min, yawrate max]
+        dw = [max(Vs[0], Vd[0]), min(Vs[1], Vd[1]),
+            max(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
 
-def calc_dynamic_window(x, config):
-
-    # Dynamic window from robot specification
-    Vs = [config.min_speed, config.max_speed,
-          -config.max_yawrate, config.max_yawrate]
-
-    # Dynamic window from motion model
-    Vd = [x[3] - config.max_accel * config.dt,
-          x[3] + config.max_accel * config.dt,
-          x[4] - config.max_dyawrate * config.dt,
-          x[4] + config.max_dyawrate * config.dt]
-    #  print(Vs, Vd)
-
-    #  [vmin,vmax, yawrate min, yawrate max]
-    dw = [max(Vs[0], Vd[0]), min(Vs[1], Vd[1]),
-          max(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
-
-    return dw
+        return dw
 
 
-def calc_trajectory(xinit, v, y, config):
+    def calc_trajectory(self, xinit, v, y, config):
 
-    x = np.array(xinit)
-    traj = np.array(x)
-    time = 0
-    while time <= config.predict_time:
-        x = motion(x, [v, y], config.dt)
-        traj = np.vstack((traj, x))
-        time += config.dt
+        x = np.array(xinit)
+        traj = np.array(x)
+        time = 0
+        while time <= config.predict_time:
+            x = motion(x, [v, y], config.dt)
+            traj = np.vstack((traj, x))
+            time += config.dt
 
-    return traj
-
-
-def calc_final_input(x, u, dw, config, goal, ob):
-
-    xinit = x[:]
-    min_cost = 10000.0
-    min_u = u
-    min_u[0] = 0.0
-    best_traj = np.array([x])
-
-    # evalucate all trajectory with sampled input in dynamic window
-    for v in np.arange(dw[0], dw[1], config.v_reso):
-        for y in np.arange(dw[2], dw[3], config.yawrate_reso):
-            traj = calc_trajectory(xinit, v, y, config)
-
-            # calc cost
-            to_goal_cost = calc_to_goal_cost(traj, goal, config)
-            speed_cost = config.speed_cost_gain * \
-                (config.max_speed - traj[-1, 3])
-            ob_cost = calc_obstacle_cost(traj, ob, config)
-            #print(ob_cost)
-
-            final_cost = to_goal_cost + speed_cost + ob_cost
-
-            # search minimum trajectory
-            if min_cost >= final_cost:
-                min_cost = final_cost
-                min_u = [v, y]
-                best_traj = traj
-
-    return min_u, best_traj
+        return traj
 
 
-def calc_obstacle_cost(traj, ob, config):
-    # calc obstacle cost inf: collistion, 0:free
+    def calc_final_input(self, x, u, dw, config, goal, ob):
 
-    skip_n = 2
-    minr = float("inf")
+        xinit = x[:]
+        min_cost = 10000.0
+        min_u = u
+        min_u[0] = 0.0
+        best_traj = np.array([x])
 
-    for ii in range(0, len(traj[:, 1]), skip_n):
-        for i in range(len(ob[:, 0])):
-            ox = ob[i, 0]
-            oy = ob[i, 1]
-            dx = traj[ii, 0] - ox
-            dy = traj[ii, 1] - oy
+        # evalucate all trajectory with sampled input in dynamic window
+        for v in np.arange(dw[0], dw[1], config.v_reso):
+            for y in np.arange(dw[2], dw[3], config.yawrate_reso):
+                traj = calc_trajectory(xinit, v, y, config)
 
-            r = math.sqrt(dx**2 + dy**2)
-            if r <= config.robot_radius:
-                return float("Inf")  # collision
+                # calc cost
+                to_goal_cost = calc_to_goal_cost(traj, goal, config)
+                speed_cost = config.speed_cost_gain * \
+                    (config.max_speed - traj[-1, 3])
+                ob_cost = calc_obstacle_cost(traj, ob, config)
+                #print(ob_cost)
 
-            if minr >= r:
-                minr = r
+                final_cost = to_goal_cost + speed_cost + ob_cost
 
-    return 1.0 / minr  # OK
+                # search minimum trajectory
+                if min_cost >= final_cost:
+                    min_cost = final_cost
+                    min_u = [v, y]
+                    best_traj = traj
 
-
-def calc_to_goal_cost(traj, goal, config):
-    # calc to goal cost. It is 2D norm.
-
-    goal_magnitude = math.sqrt(goal[0]**2 + goal[1]**2)
-    traj_magnitude = math.sqrt(traj[-1, 0]**2 + traj[-1, 1]**2)
-    dot_product = (goal[0]*traj[-1, 0]) + (goal[1]*traj[-1, 1])
-    error = dot_product / (goal_magnitude*traj_magnitude)
-    error_angle = math.acos(error)
-    cost = config.to_goal_cost_gain * error_angle
-
-    return cost
+        return min_u, best_traj
 
 
-def dwa_control(x, u, config, goal, ob):
-    # Dynamic Window control
+    def calc_obstacle_cost(self, traj, ob, config):
+        # calc obstacle cost inf: collistion, 0:free
 
-    dw = calc_dynamic_window(x, config)
+        skip_n = 2
+        minr = float("inf")
 
-    u, traj = calc_final_input(x, u, dw, config, goal, ob)
+        for ii in range(0, len(traj[:, 1]), skip_n):
+            for i in range(len(ob[:, 0])):
+                ox = ob[i, 0]
+                oy = ob[i, 1]
+                dx = traj[ii, 0] - ox
+                dy = traj[ii, 1] - oy
 
-    return u, traj
+                r = math.sqrt(dx**2 + dy**2)
+                if r <= config.robot_radius:
+                    return float("Inf")  # collision
+
+                if minr >= r:
+                    minr = r
+
+        return 1.0 / minr  # OK
 
 
-def plot_arrow(x, y, yaw, length=0.5, width=0.1):
-    plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
-              head_length=width, head_width=width)
-    plt.plot(x, y)
+    def calc_to_goal_cost(self, traj, goal, config):
+        # calc to goal cost. It is 2D norm.
 
-def loop(robot_pos, goal_pos):
-    x = np.array([robot_pos.x, robot_pos.y, math.pi / robot_rot.])
+        goal_magnitude = math.sqrt(goal[0]**2 + goal[1]**2)
+        traj_magnitude = math.sqrt(traj[-1, 0]**2 + traj[-1, 1]**2)
+        dot_product = (goal[0]*traj[-1, 0]) + (goal[1]*traj[-1, 1])
+        error = dot_product / (goal_magnitude*traj_magnitude)
+        error_angle = math.acos(error)
+        cost = config.to_goal_cost_gain * error_angle
+
+        return cost
+
+
+    def dwa_control(self, x, u, config, goal, ob):
+        # Dynamic Window control
+
+        dw = calc_dynamic_window(x, config)
+
+        u, traj = calc_final_input(x, u, dw, config, goal, ob)
+
+        return u, traj
+
+
+    def plot_arrow(self, x, y, yaw, length=0.5, width=0.1):
+        plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
+                head_length=width, head_width=width)
+        plt.plot(x, y)
+
+    def loop(self):
+        self.u, ltraj = self.dwa_control(self.x, self.u, self.config, self.goal, self.ob)
+        # check goal
+        if math.sqrt((self.x[0] - self.goal[0])**2 + (self.x[1] - self.goal[1])**2) <= self.config.robot_radius:
+            print("Goal!!")
+        return self.u
 
 def main():
-    print(__file__ + " start!!")
+    # print(__file__ + " start!!")
     # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
     x = np.array([0.0, 0.0, math.pi / 8.0, 0.0, 0.0])
     # goal position [x(m), y(m)]
@@ -186,10 +196,15 @@ def main():
     traj = np.array(x)
 
     for i in range(1000):
+        # ob[0] = []
+
         u, ltraj = dwa_control(x, u, config, goal, ob)
+        # print(u)
 
         x = motion(x, u, config.dt)
         traj = np.vstack((traj, x))  # store state history
+        # if i <= 5:
+        #   print(traj)
 
         if show_animation:
             plt.cla()
--- light_follower/light_follower_node.py
+++ light_follower/light_follower_node.py
@@ -5,6 +5,8 @@ from tf2_msgs.msg import TFMessage
 import copy
 from tf2_ros import Buffer, TransformListener, LookupException, ConnectivityException, ExtrapolationException
 import tf2_geometry_msgs
+import numpy as np
+from . import dwa
 
 TIME_PERIOD = 0.1
 NUM_ROBOT = 4
@@ -13,10 +15,13 @@ class calc_relative_pos:
     def __init__(self):
         pass
 
-    def 
+    # def 
         
 class robot_control:
-    pass
+    def __init__(self):
+        pass
+    
+    def 
 
 class light_follower_node(Node):
     def __init__(self):
@@ -25,8 +30,8 @@ class light_follower_node(Node):
         self.inital_pose_sub = self.create_subscription(PoseWithCovarianceStamped, 'initialpose', self.initial_pose_callback, rclpy.qos.qos_profile_sensor_data)
 
         self.get_pos_list = [0 for i in range(NUM_ROBOT)]
-        self.get_rot_list = [0 for i in range(NUM_ROBOT)]
         self.get_yaw_list = [0 for i in range(NUM_ROBOT)]
+        self.get_u_list = [0 for i in range(NUM_ROBOT)]
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
@@ -41,11 +46,42 @@ class light_follower_node(Node):
         self.init = False
 
         # goal pose 
-        self.get_pos = 0
+        self.goal_pos = 0
+
+        # instance dwa
+        self.dwa = dwa.dwa_approach()
+
+        # robot_control
+        self.u_list = [0 for i in range(NUM_ROBOT)]
+        for i in range(NUM_ROBOT):
+            self.u_list[i] = np.array([0.0, 0.0])
+
+        self.ob = [0 for i in range(NUM_ROBOT-1)]
+
+        self.goal_flag = False
     
     def initial_pose_callback(self, msg):
-        self.get_pos = copy.deepcopy(msg)
-        return self.get_pos
+        self.goal_pos = copy.deepcopy(msg)
+        return self.goal_pos
+
+    def euler_from_quaternion(self, quaternion):
+        x = quaternion.x
+        y = quaternion.y
+        z = quaternion.z
+        w = quaternion.w
+
+        sinr_cosp = 2 * (w * x + y * z)
+        cosr_cosp = 1 - 2 * (x * x + y * y)
+        roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+        sinp = 2 * (w * y - z * x)
+        pitch = np.arcsin(sinp)
+
+        siny_cosp = 2 * (w * z + x * y)
+        cosy_cosp = 1 - 2 * (y * y + z * z)
+        yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+        return roll, pitch, yaw
 
     def timer_callback(self):
         try:
@@ -53,13 +89,25 @@ class light_follower_node(Node):
                 transform = self.tf_buffer.lookup_transform('robot' + str(i+1), 'map', rclpy.time.Time())
                 position = transform.transform.translation
                 rotation = transform.transform.rotation
+                (roll, pitch, yaw) = self.euler_from_quaternion(rotation)
+                self.ob[i] = copy.deepcopy(np.array[position.x, position.y])
                 self.get_pos_list[i] = copy.deepcopy(position)
-                self.get_rot_list[i] = copy.deepcopy(rotation)
+                self.get_yaw_list[i] = copy.deepcopy(yaw)
             self.init = True
 
-            # test
-            if self.init:
-                print(self.get_pos_list[0].x)
+            if self.init and self.goal_flag:
+                # debug
+                # print(self.get_yaw_list)
+                # print(self.goal_pos_list[0].x)
+
+                # for i in range(NUM_ROBOT):
+                self.dwa.x = np.array([self.get_pos_list[i].x, self.get_pos_list[i].y, self.get_yaw_list[i], self.u_list[0], self.u_list[1]])
+                self.dwa.goal_pos = np.array([self.goal[0], self.goal[1]])
+                ob_ = copy.deepcopy(self.ob)
+                np.delete(ob_, i, 0)
+                self.dwa.ob = ob_
+                self.dwa.u = self.u_list[i]
+                self.u[i] = self.dwa.loop()
 
         except (LookupException, ConnectivityException, ExtrapolationException) as e:
             self.get_logger().info(f'Exception: {e}')
